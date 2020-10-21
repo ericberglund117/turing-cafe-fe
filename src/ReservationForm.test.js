46184c1cb2e0a6678f4b5467eb1a5cd8eefb68e6
Eric Berglund <ericberglund117@gmail.com>
2020-10-21 11:54:31 -0600

    Pass value change in ReservationForm test

--- src/ReservationForm.test.js
+++ src/ReservationForm.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReservationForm from './ReservationForm'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom';
 
 const mockAddReservation = jest.fn();
 
 describe('Make a Reservation', () => {
   it('Should render a reservation input field', () => {
-    render(<ReservationForm addReservation={ mockAddReservation } />)
+    render(<ReservationForm addReservation={ mockAddReservation } />);
 
     const reservationContainer = screen.getByTitle('make-reservation');
     const nameInput = screen.getByPlaceholderText('Name');
@@ -23,6 +23,21 @@ describe('Make a Reservation', () => {
   })
 
   it('should change input values when a user fills out a reservation', () => {
-    render(<ReservationForm addReservation={ mockAddReservation } />)
+    render(<ReservationForm addReservation={ mockAddReservation } />);
+
+    const nameInput = screen.getByPlaceholderText('Name');
+    const dateInput = screen.getByPlaceholderText('Date(mm/dd)');
+    const timeInput = screen.getByPlaceholderText('Time');
+    const numberInput = screen.getByPlaceholderText('Number of guests');
+
+    nameInput.value = "Mike Meyers";
+    dateInput.value = '10/31';
+    timeInput.value = '12:00';
+    numberInput.value = 1;
+
+    fireEvent.change(nameInput);
+    fireEvent.change(dateInput);
+    fireEvent.change(timeInput);
+    fireEvent.change(numberInput);
   })
 })
