6b01483efe6840f240eb94fce9beb2fbe6a0a20c
Eric Berglund <ericberglund117@gmail.com>
2020-10-21 11:51:30 -0600

    Pass input display in ReservationForm test

--- src/ReservationForm.test.js
+++ src/ReservationForm.test.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import ReservationForm from './ReservationForm'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom';
+
+const mockAddReservation = jest.fn();
+
+describe('Make a Reservation', () => {
+  it('Should render a reservation input field', () => {
+    render(<ReservationForm addReservation={ mockAddReservation } />)
+
+    const reservationContainer = screen.getByTitle('make-reservation');
+    const nameInput = screen.getByPlaceholderText('Name');
+    const dateInput = screen.getByPlaceholderText('Date(mm/dd)');
+    const timeInput = screen.getByPlaceholderText('Time');
+    const numberInput = screen.getByPlaceholderText('Number of guests');
+
+    expect(reservationContainer).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+    expect(dateInput).toBeInTheDocument();
+    expect(timeInput).toBeInTheDocument();
+    expect(numberInput).toBeInTheDocument();
+  })
+
+  it('should change input values when a user fills out a reservation', () => {
+    render(<ReservationForm addReservation={ mockAddReservation } />)
+  })
+})
