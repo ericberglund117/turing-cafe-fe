176775bed2cd77e6126c374b15feeb393b71f684
Eric Berglund <ericberglund117@gmail.com>
2020-10-21 11:42:42 -0600

    Pass ReservationCard test

--- src/ReservationCards.test.js
+++ src/ReservationCards.test.js
@@ -6,13 +6,6 @@ import '@testing-library/jest-dom';
 describe('Reservation Cards', () => {
   it('should render a reservation card', () => {
     const reservations = [{
-      id: 18907224,
-      name: 'Christie',
-      date: '8/8',
-      time: '7:00',
-      number: 3
-      },
-      {
         id: 18907223,
         name: 'Mike Meyers',
         date: '10/31',
@@ -22,10 +15,10 @@ describe('Reservation Cards', () => {
 
     render(<ReservationCards reservations={reservations} />)
 
-    const name = screen.getByText('Christie')
-    const date = screen.getByText('8/8')
-    const time = screen.getByText('7:00')
-    const number = screen.getByText('3')
+    const name = screen.getByText('Mike Meyers')
+    const date = screen.getByText('10/31')
+    const time = screen.getByText('12:00')
+    const number = screen.getByText('Number of Guests: 1')
     expect(name).toBeInTheDocument();
     expect(date).toBeInTheDocument();
     expect(time).toBeInTheDocument();
